e0f46d971c6f53f7701ff0e4e09e76bd95af4e28
Levi Sitters <lsitters@gmail.com>
2016-08-11 15:39:23 -0500

    Entry point created

---
+++ OauthExample/js/auth/loginConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DISCOGS_CONFIG: {
+        Consumer_Key: "EjZkjtrxYgiCUsxLUumZ",
+        Consumer_Secret: "WuRoIzWMJQERLWfoLaVkaDgRXfZgPNVp%26",
+        Request_Token_URL: "https://api.discogs.com/oauth/request_token",
+        Authorize_URL: "https://www.discogs.com/oauth/authorize",
+        Access_Token_URL: "https://api.discogs.com/oauth/access_token"
+    }
+}
